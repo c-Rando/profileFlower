2d90ace912e7e7443eba78cf9e9addd8ad3f2c82
C. Rando <crando@gmail.com>
2021-06-24 15:39:33 -0400

    Complete cmdline application

--- index.js
+++ index.js
@@ -1,78 +1,154 @@
+const Manager = require("./dependencies/lib/Manager");
+const Engineer = require("./dependencies/lib/Engineer");
+const Intern = require("./dependencies/lib/Intern");
+const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
-const inquirer = require("inquirer");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const Manager = require("./assets/dependencies/lib/Manager-starter");
-const Intern = require("./assets/dependencies/lib/Intern-starter");
-const Engineer = require("./assets/dependencies/lib/Engineer-starter");
-
-const outputDirectory = path.resolve(__dirname, "output")
-const outputPath = path.join(outputDirectory, "renderedTeamSheet.html");
-const renderer = require("./assets/dependencies/src/htmlRenderer");
-
-//obj return make ana rray of teamembers , 
+const render = require("./dependencies/src/htmlRenderer");
 
 const teamMembers = [];
+const idArray = [];
 
-// .push() a new employee for each time they are created and loop over the 
-// array for each time one is needed
+async function promptForUniqueId (answers) {
+  while (idArray.includes(answers.id)) {
+    const { id } = await inquirer.prompt([
+      {
+        name: 'id',
+        message: "Id is already used. Please insert another id"
+      }
+    ]);
+    answers.id = id;
+  }
+}
 
-function createManager() {
+function appMenu() {
+
+  function createManager() {
+    console.log("Please build your team");
     inquirer.prompt([
-        {
-            type: "input",
-            name: "managerName",
-            message: "What is the Manager's name?",
-            //validate: ,
-        },
-        {
-            type: "input",
-            name: "managerId",
-            message: "What is the Manager's id?",
-            //validate: ,
-        },
-        {
-            type: "input",
-            name: "managerEmail",
-            message: "What is the Manager's email address?",
-            //validate: ,
-        },
-        {
-            type: "input",
-            name: "officeNumber",
-            message: "What is the Manager's office number?",
-            //validate: ,
-        },
-    ]) // represents the object that you are getting back
-    .then((answers) => {
-        const manager = new Manager (answers.managerName, answers.managerid, answers.officeNumber, answers.managerEmail);
-        teamMembers.push(manager);
-        console.log(teamMembers);
-        //createTeam(); = asks user what tyoe of team member would like to add. inquirer with choices property, switch case, if engineer, run the engineer questions
-    })
-    // .catch(err => {
-    //     if (err) {
-    //         // Prompt couldn't be rendered in the current environment
-    //       } else {
-    //         // Something else went wrong
-    //       }
-    // });
-};
+      {
+        name: 'name',
+        message: "Please type the manager name"
+      },
+      {
+        name: 'id',
+        message: "Please type the engineer id"
+      },
+      {
+        name: 'email',
+        message: "Please type the manager email"
+      },
+      {
+        name: 'officeNumber',
+        message: "Please type the manager office number"
+      }
+    ]).then(async (answers) => {
+      console.log(answers);
+      const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+      teamMembers.push(manager);
+      idArray.push(answers.id);
+      createTeam();
+    });
+  }
 
-createManager();
+  function createTeam() {
 
-// function createTeam() {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "memberChoice",
+        message: "Which type of team member would you like to add?",
+        choices: [
+          "Engineer",
+          "Intern",
+          "I don't want to add any more team members"
+        ]
+      }
+    ]).then(userChoice => {
+      switch(userChoice.memberChoice) {
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+      default:
+        buildTeam();
+      }
+    });
+  }
 
-//     switch (expression) { // expression  or inp param would be === to user choice .. engineer, intern
-//         case x:
-//             // code block
-//             break;
-//         case y:
-//             // code block
-//             break;
-//         default:
-//         // code block
-//     };
-// };
+  function addEngineer() {
+    inquirer.prompt([
+      {
+        name: 'name',
+        message: "Please type the engineer name"
+      },
+      {
+        name: 'id',
+        message: "Please type the engineer id"
+      },
+      {
+        name: 'email',
+        message: "Please type the engineer email"
+      },
+      {
+        name: 'github',
+        message: "Please type the engineer github profile"
+      }
+    ]).then(async (answers) => {
+      console.log(answers);
+      await promptForUniqueId(answers);
+      const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+      teamMembers.push(engineer);
+      idArray.push(answers.id);
+      createTeam();
+    });
+  }
 
+  function addIntern() {
+    inquirer.prompt([
+      {
+        name: 'name',
+        message: "Please type the intern name"
+      },
+      {
+        name: 'id',
+        message: "Please type the intern id"
+      },
+      {
+        name: 'email',
+        message: "Please type the intern email"
+      },
+      {
+        name: 'github',
+        message: "Please type the intern school"
+      }
+    ]).then(async (answers) => {
+      console.log(answers);
+      await promptForUniqueId(answers);
+      const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
+      teamMembers.push(intern);
+      idArray.push(answers.id);
+      createTeam();
+    });
+  }
+
+  function buildTeam() {
+    // Create the output directory if the output path doesn't exist
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+  }
+
+  createManager();
+
+}
+
+
+appMenu();
